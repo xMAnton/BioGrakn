f07e26ee1f873d800aa2b6bf6fd0212bdd5915ad
Antonio Messina <messina@pa.icar.cnr.it>
2017-04-05 13:39:48 +0200

    Add copyright notices

--- src/main/java/it/cnr/icar/biograkn/_09_Cancer2Mirna.java
+++ src/main/java/it/cnr/icar/biograkn/_09_Cancer2Mirna.java
@@ -1,3 +1,21 @@
+/*
+ * BioGrakn - A Knowledge Graph-based Semantic Database for Biomedical Sciences
+ * Copyright (C) 2017 - Antonio Messina (xMAnton) <antonio.messina@icar.cnr.it>
+ *
+ * BioGrakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BioGrakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BioGrakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
 package it.cnr.icar.biograkn;
 
 import java.io.BufferedReader;
